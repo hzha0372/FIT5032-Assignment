9a0ba9aa4b5897a6500a1411e2e8270c7b37aa75
zhanghongrui <hzha0372@student.monash.edu>
2025-09-12 08:09:23 +0800

    a1.3

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,8 +1,7 @@
 <template>
   <div style="width: 350px; margin: 50px auto">
-    <h2 style="margin-bottom: 20px">Sign in</h2>
+    <h2 style="margin-bottom: 20px">Login</h2>
     <form @submit.prevent="submitForm">
-      <!-- Email -->
       <div style="margin-bottom: 20px">
         <label>Email:</label><br />
         <input type="text" v-model="email" style="width: 100%; padding: 5px" />
@@ -11,7 +10,6 @@
         </p>
       </div>
 
-      <!-- Password -->
       <div style="margin-bottom: 20px">
         <label>Password:</label><br />
         <input type="password" v-model="password" style="width: 100%; padding: 5px" />
@@ -20,7 +18,20 @@
         </p>
       </div>
 
-      <button type="submit" style="margin-top: 20px; padding: 5px 15px">Sign in</button>
+      <div style="margin-bottom: 20px">
+        <label>Please select your role:</label><br />
+        <label style="display: block; margin-top: 5px">
+          <input type="radio" value="teen" v-model="selectedRole" />
+          I'm a young person
+        </label>
+        <label style="display: block; margin-top: 5px">
+          <input type="radio" value="staff" v-model="selectedRole" />
+          I'm supporting a young person
+        </label>
+        <p v-if="roleError" style="color: red; margin: 5px 0 0">{{ roleError }}</p>
+      </div>
+
+      <button type="submit" style="margin-top: 20px; padding: 5px 15px">Login</button>
     </form>
 
     <div style="margin-top: 50px" v-if="users.length > 0">
@@ -28,6 +39,7 @@
       <DataTable :value="users">
         <Column field="email" header="Email"></Column>
         <Column field="password" header="Password"></Column>
+        <Column field="role" header="Role"></Column>
       </DataTable>
     </div>
   </div>
@@ -37,11 +49,15 @@
 import { ref } from 'vue'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { useRouter } from 'vue-router'
-
+import isAuthenticated from '@/authenticate'
 const email = ref('')
 const password = ref('')
+const selectedRole = ref('')
+
 const emailError = ref('')
 const passwordError = ref('')
+const roleError = ref('')
+
 const users = ref([])
 
 const auth = getAuth()
@@ -64,17 +80,26 @@ const submitForm = () => {
     passwordError.value = ''
   }
 
-  if (emailError.value === '' && passwordError.value === '') {
+  if (selectedRole.value === '') {
+    roleError.value = 'Please select a role.'
+  } else {
+    roleError.value = ''
+  }
+
+  if (emailError.value === '' && passwordError.value === '' && roleError.value === '') {
     signInWithEmailAndPassword(auth, email.value, password.value)
       .then((userCredential) => {
         alert('Login successful!')
+        isAuthenticated.value = true
         users.value.push({
           email: email.value,
           password: password.value,
+          role: selectedRole.value,
         })
         email.value = ''
         password.value = ''
-        router.push('/')
+        const redirectPath = selectedRole.value === 'teen' ? '/TeenPage' : '/StaffPage'
+        router.push(redirectPath)
       })
       .catch((error) => {
         if (error.code === 'auth/user-not-found') {
